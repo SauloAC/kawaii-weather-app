0f1c2e4e8dd725ff179c547781474d19fb2b007c
SauloAC <saulo.andrade.costa@gmail.com>
2025-09-25 13:25:53 -0700

    Create getForecastFiveDays function

---
+++ src/public/js/getForecastFiveDays.js
@@ -0,0 +1,24 @@
+import { getIconByWeather } from "./getIconByWeather";
+import { hourlyForecast } from "./hourlyForecast";
+
+// display five days forecast
+export const getForecastFiveDays = (data) => {
+  for (i = 0; i < 5; i++) {
+    document.querySelector(`#day${i + 1}`).innerHTML = `<h3 id="day${
+      i + 1
+    }Date">${data[i * 8].month} ${data[i * 8].day}</h3><img id="day${
+      i + 1
+    }img" src" " alt="Current Weather Information"><p id="day${
+      i + 1
+    }Temp">${Math.round(data[i * 8].temp)}°C</p><p id="day${
+      i + 1
+    }MinTemp">Min: ${Math.round(data[i * 8].temp_min)}°C</p><p id="day${
+      i + 1
+    }MaxTemp">Max: ${Math.round(data[i * 8].temp_max)}°C</p>`;
+    getIconByWeather(
+      data[i * 8].description,
+      document.getElementById(`day${i + 1}img`)
+    );
+  }
+  hourlyForecast(data);
+};
