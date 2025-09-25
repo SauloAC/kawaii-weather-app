959fe939add0f7623ed9133385dcb53aedfc4796
SauloAC <saulo.andrade.costa@gmail.com>
2025-09-25 14:27:34 -0700

    Create errorCallBack function

---
+++ src/public/js/errorCallBack.js
@@ -0,0 +1,12 @@
+import { cityName, lat, pwChecker } from "./data";
+import { weatherCheck } from "./weatherCheck";
+
+export const errorCallBack = async () => {
+  // Setting Vancouver as default city if user denies Location access
+  cityName = `Vancouver, BC, Canada`;
+  lat = 49.24;
+  lon = -123.11;
+  // when reload the page, every information change to default city
+  pwChecker++;
+  weatherCheck();
+};
